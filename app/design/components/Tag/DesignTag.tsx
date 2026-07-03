3979df9c89bbb263265dc5b112257c701d8220e0
Krisha <krisha9845@gmail.com>
2026-07-02 22:43:32 -0400

    remove unused function

--- app/design/components/Tag/DesignTag.tsx
+++ app/design/components/Tag/DesignTag.tsx
@@ -1,14 +1,3 @@
-export function DesignTag(tag: string) {
-  return (
-    <span
-      key={tag}
-      className="inline-block px-3 py-0.5 rounded-full text-[15px] text-[#f6f7ed] bg-[#456395]"
-    >
-      {tag}
-    </span>
-  );
-}
-
 export default function DesignTagList({ tags }: { tags: string[] }) {
   return (
     <>
